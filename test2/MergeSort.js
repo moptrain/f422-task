9cf1f46e260a4ef1a7b403874aad35e8d780e4bd
MopTra <1053978204@qq.com>
2018-09-30 16:13:52 +0800

    more es6

--- test2/MergeSort.js
+++ test2/MergeSort.js
@@ -1,28 +1,27 @@
 
 
 
-T=[3,44,38,5,47,15,36,26,27,2,46,4,19,50,48];
-TMP=T.concat();
+let T=[3,44,38,5,47,15,36,26,27,2,46,4,19,50,48];
+let TMP=T.concat();
 
 function msort(l,r) {
-    console.log(l,r)
-    if (l==r){
+    if (l===r){
         TMP[l]=T[l];
         return;
     }
     var mid=parseInt((l+r)/2);
     msort(l,mid);
     msort(mid+1,r);
-    var i;
-    for (i=1;i<=mid;i++)
-        TMP[i]=T[i]
-    var k=r;
+    for (let i=1;i<=mid;i++)
+        TMP[i]=T[i];
+    let k=r;
     for (i=mid+1;i<=r;i++){
         TMP[i]=T[k];
         k--;
     }
-    var ll=l,rr=r;
-    var kk
+    //下面正式开始归并
+    let ll=l,rr=r;
+    let kk;
     for (kk=l;kk<=r;kk++){
         if (TMP[ll]<TMP[rr]){
             T[kk]=TMP[ll];
--- test2/qSort.js
+++ test2/qSort.js
@@ -1,26 +1,26 @@
 function cmp(a,b) {
     return (a<b)
 }
-function qsort(A,l,r,fun) {
+var qsort=function (A,l,r,fun) {
     //快排随机化开始
-    var ran=parseInt(Math.random()*1000000%(r-l))
+    let ran=parseInt(Math.random()*1000000%(r-l));
     ran=l+ran;
-    var t=A[ran];A[ran]=A[l];A[l]=t;
+    let t=A[ran];A[ran]=A[l];A[l]=t;
     //快排随机化结束
-    var pivot=A[l]
+    var pivot=A[l];
     var index=l+1;
-    for (var i=l+1;i<=r;i++){
+    for (let i=l+1;i<=r;i++){
         if (fun(A[i],pivot)){
-            var tmp=A[i];A[i]=A[index];A[index]=tmp;
+            let tmp=A[i];A[i]=A[index];A[index]=tmp;
             index=index+1
         }
     }
     //swap
-    var t=A[l];A[l]=A[index-1];A[index-1]=t;
+    t=A[l];A[l]=A[index-1];A[index-1]=t;
 
-    if (index-2-l+1>1) qsort(A,l,index-2,fun)
-    if (r-index+1>1) qsort(A,index,r,fun)
-}
-T=[3,44,38,5,47,15,36,26,27,2,46,4,19,50,48,48,48]
-qsort(T,0,16,cmp)
-console.log(T)
+    if (index-2-l+1>1) qsort(A,l,index-2,fun);
+    if (r-index+1>1) qsort(A,index,r,fun);
+};
+let test_case=[3,44,38,5,47,15,36,26,27,2,46,4,19,50,48,48,48];
+qsort(test_case,0,16,cmp);
+console.log(test_case);
